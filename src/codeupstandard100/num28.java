1bf6de3c50dbd68747a1aed50cf96c30c82744e9
y3l512 <yl128560@swu.ac.kr>
2022-05-20 12:55:56 +0900

    commit test

---
+++ src/codeupstandard100/num28.java
@@ -0,0 +1,20 @@
+package codeupstandard100;
+import java.util.*;
+
+
+public class num28 {
+
+	public static void main(String[] args) {
+		long a;
+        Scanner sn = new Scanner(System.in);
+        a = sn.nextLong();
+        System.out.println(a);
+//		 double a;
+//	        Scanner sn = new Scanner(System.in);
+//	        a = sn.nextDouble();
+//	        
+//	        System.out.printf("%.11f", a);
+
+	}
+
+}
---
+++ src/codeupstandard100/num4.java
@@ -0,0 +1,7 @@
+package codeupstandard100;
+
+public class num4 {
+	public static void main(String[] args) {
+		System.out.println("\"!@#$%^&*()\"");
+	}
+}
